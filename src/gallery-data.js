c4feff4b3f86f9bc19b8dd9a0cdfeb4c969022f6
Billy Orr <thebillyorr@gmail.com>
2025-10-29 00:48:23 -0700

    increase hero page photos to 34

--- src/gallery-data.js
+++ src/gallery-data.js
@@ -53,5 +53,155 @@ export const GALLERY_DATA = [
     src: 'assets/img/img-9.jpg',
     alt: 'Project or work sample 9',
     caption: '📍 Batu Caves, Malaysia - Huge statue at the cave bottom'
+  },
+  {
+    id: 10,
+    src: 'assets/img/img-10.jpg',
+    alt: 'Project or work sample 10',
+    caption: '📍 Ninh Binh, Vietnam - View of the flooded rice paddies'
+  },
+  {
+    id: 11,
+    src: 'assets/img/img-11.jpg',
+    alt: 'Project or work sample 11',
+    caption: '📍  Ninh Binh, Vietnam - Motorcycle ride through the village'
+  },
+  {
+    id: 12,
+    src: 'assets/img/img-12.jpg',
+    alt: 'Project or work sample 12',
+    caption: '📍 Taoyuan, Taiwan - Biking along the street'
+  },
+  {
+    id: 13,
+    src: 'assets/img/img-13.jpg',
+    alt: 'Project or work sample 13',
+    caption: '📍 Kenting, Taiwan - Riding up the eastern coast'
+  },
+  {
+    id: 14,
+    src: 'assets/img/img-14.jpg',
+    alt: 'Project or work sample 14',
+    caption: '📍 Phuket, Thailand - Protected beaches'
+  },
+  {
+    id: 15,
+    src: 'assets/img/img-15.jpg',
+    alt: 'Project or work sample 15',
+    caption: '📍 Bali, Indonesia - Downstream from the waterfall'
+  },
+  {
+    id: 16,
+    src: 'assets/img/img-16.jpg',
+    alt: 'Project or work sample 16',
+    caption: '📍 Okinawa, Japan - View from the outskirts of Naha'
+  },
+  {
+    id: 17,
+    src: 'assets/img/img-17.jpg',
+    alt: 'Project or work sample 17',
+    caption: '📍 Taoyuan, Taiwan - Biking through the village'
+  },
+  {
+    id: 18,
+    src: 'assets/img/img-18.jpg',
+    alt: 'Project or work sample 18',
+    caption: '📍 Okinawa, Japan - Secluded Beach'
+  },
+  {
+    id: 19,
+    src: 'assets/img/img-19.jpg',
+    alt: 'Project or work sample 19',
+    caption: '📍 Chiayi, Taiwan - Biking on the boardwalk'
+  },
+  {
+    id: 20,
+    src: 'assets/img/img-20.jpg',
+    alt: 'Project or work sample 20',
+    caption: '📍 Okinawa, Japan - Bright blue waters'
+  },
+  {
+    id: 21,
+    src: 'assets/img/img-21.jpg',
+    alt: 'Project or work sample 21',
+    caption: '📍 Ein Gedi, Israel - Early morning hike'
+  },
+  {
+    id: 22,
+    src: 'assets/img/img-22.jpg',
+    alt: 'Project or work sample 22',
+    caption: '📍 Sea of Galilee, Israel - Cows grazing on the hills'
+  },
+  {
+    id: 23,
+    src: 'assets/img/img-23.jpg',
+    alt: 'Project or work sample 23',
+    caption: '📍 Barcelona, Spain - Park sculptures'
+  },
+  {
+    id: 24,
+    src: 'assets/img/img-24.jpg',
+    alt: 'Project or work sample 24',
+    caption: '📍 Edinburgh, Scotland - View of Arthurs Seat'
+  },
+  {
+    id: 25,
+    src: 'assets/img/img-25.jpg',
+    alt: 'Project or work sample 25',
+    caption: '📍 Edinburgh, Scotland - Bike trip through the city'
+  },
+  {
+    id: 26,
+    src: 'assets/img/img-26.jpg',
+    alt: 'Project or work sample 26',
+    caption: '📍 Stanley, Hong Kong - View of the southern edge'
+  },
+  {
+    id: 27,
+    src: 'assets/img/img-27.jpg',
+    alt: 'Project or work sample 27',
+    caption: '📍 Aberdeen, Hong Kong - Fishing village'
+  },
+  {
+    id: 28,
+    src: 'assets/img/img-28.jpg',
+    alt: 'Project or work sample 28',
+    caption: '📍 New Delhi, India - Brutalist Architecture'
+  },
+  {
+    id: 29,
+    src: 'assets/img/img-29.jpg',
+    alt: 'Project or work sample 29',
+    caption: '📍 Taichung, Taiwan - Lots of wheat fields'
+  },
+  {
+    id: 30,
+    src: 'assets/img/img-30.jpg',
+    alt: 'Project or work sample 30',
+    caption: '📍 Nara, Japan - Quiet temple grounds'
+  },
+  {
+    id: 31,
+    src: 'assets/img/img-31.jpg',
+    alt: 'Project or work sample 31',
+    caption: '📍 Ninh Binh, Vietnam - Staggering rock cliffs'
+  },
+  {
+    id: 32,
+    src: 'assets/img/img-32.jpg',
+    alt: 'Project or work sample 32',
+    caption: '📍 Sun Moon Lake, Taiwan - Biking through the forest'
+  },
+  {
+    id: 33,
+    src: 'assets/img/img-33.jpg',
+    alt: 'Project or work sample 33',
+    caption: '📍 Puli, Taiwan - Valley Bridge'
+  },
+  {
+    id: 34,
+    src: 'assets/img/img-34.jpg',
+    alt: 'Project or work sample 34',
+    caption: '📍 Taichung, Taiwan - Night time walk'
   }
 ];
